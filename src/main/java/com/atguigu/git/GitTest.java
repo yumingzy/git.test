24414cbba3cc57a70d66be0c4294c8e029a1b72b
yumingzy <1285840413@qq.com>
2026-05-15 22:13:13 +0800

    idea pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("idea push test");
+        System.out.println("idea pull test")
     }
 }
