3e26839174b573d3a41200b9179448472a178416
Administrator <gitlab_admin_bacec2@example.com>
2026-05-16 10:26:02 +0000

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("idea push gitlab commit");
         System.out.println("idea pull gitlab commit");
         System.out.println("idea push gitlab commit");
+        System.out.println("idea pull gitlab commit2");
     }
 }
