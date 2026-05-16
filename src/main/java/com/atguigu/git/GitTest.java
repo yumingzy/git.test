4d98f8a02b89ed798a403924261e4c2e2d8c6c49
zy <zy@atguigu.com>
2026-05-16 18:19:12 +0800

    idea pull gitlab commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("idea share gitee test");
         System.out.println("idea push gitee test");
         System.out.println("idea pull gitee test");
+        System.out.println("idea pull gitlab commit");
     }
 }
